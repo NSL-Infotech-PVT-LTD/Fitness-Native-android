ad3298dd5f70968dfab2ce518d6c0c6b37fc1bdc
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-09-05 11:29:47 +0530

    Setting

--- app/src/main/java/com/netscape/utrain/activities/SettingsActivity.java
+++ app/src/main/java/com/netscape/utrain/activities/SettingsActivity.java
@@ -3,14 +3,43 @@ package com.netscape.utrain.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textview.MaterialTextView;
 import com.netscape.utrain.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SwitchMaterial switchMaterial;
+    MaterialTextView textViewOn, textViewOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        switchMaterial = findViewById(R.id.settings_Switch);
+        textViewOn = findViewById(R.id.switchOn_ON);
+        textViewOff = findViewById(R.id.switchOff_OFF);
+
+        switchMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (switchMaterial.isChecked())
+                {
+                    textViewOn.setVisibility(View.VISIBLE);
+                    textViewOff.setVisibility(View.GONE);
+                } else
+                {
+                    textViewOff.setVisibility(View.VISIBLE);
+                    textViewOn.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+
     }
 }
