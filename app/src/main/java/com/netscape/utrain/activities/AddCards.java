cb90ca2ce9cf06dbee7b746da69aabeab2ee93b1
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-09-05 19:22:58 +0530

    5-09-2019

--- app/src/main/java/com/netscape/utrain/activities/AddCards.java
+++ app/src/main/java/com/netscape/utrain/activities/AddCards.java
@@ -1,18 +1,118 @@
 package com.netscape.utrain.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.netscape.utrain.R;
 import com.netscape.utrain.databinding.ActivityAddCardsBinding;
 
+import java.util.ArrayList;
+
 public class AddCards extends AppCompatActivity {
     ActivityAddCardsBinding binding;
+    AppCompatSpinner spinnerMonth,spinnerExpiryYear;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_add_cards);
+
+
+        spinnerMonth = findViewById(R.id.citySpin);
+        spinnerExpiryYear = findViewById(R.id.expiryYearSpin);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Select");
+        list.add("01");
+        list.add("02");
+        list.add("03");
+        list.add("04");
+        list.add("05");
+        list.add("06");
+        list.add("07");
+        list.add("08");
+        list.add("09");
+        list.add("10");
+        list.add("11");
+        list.add("12");
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,list);
+        spinnerMonth.setAdapter(adapter);
+
+        spinnerMonth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                spinnerMonth.setSelection(i);
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayList<String> yearList = new ArrayList<>();
+        yearList.add("Select");
+        yearList.add("2000");
+        yearList.add("2001");
+        yearList.add("2002");
+        yearList.add("2003");
+        yearList.add("2004");
+        yearList.add("2005");
+        yearList.add("2006");
+        yearList.add("2007");
+        yearList.add("2008");
+        yearList.add("2009");
+        yearList.add("2010");
+        yearList.add("2011");
+        yearList.add("2012");
+        yearList.add("2013");
+        yearList.add("2014");
+        yearList.add("2015");
+        yearList.add("2016");
+        yearList.add("2017");
+        yearList.add("2018");
+        yearList.add("2019");
+        yearList.add("2020");
+        yearList.add("2021");
+        yearList.add("2022");
+        yearList.add("2023");
+        yearList.add("2024");
+        yearList.add("2025");
+        yearList.add("2026");
+        yearList.add("2027");
+        yearList.add("2028");
+        yearList.add("2029");
+        yearList.add("2030");
+
+        ArrayAdapter yearAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,yearList);
+        spinnerExpiryYear.setAdapter(yearAdapter);
+
+        spinnerExpiryYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                spinnerExpiryYear.setSelection(i);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
