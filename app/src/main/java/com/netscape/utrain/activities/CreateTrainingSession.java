cd5cd89ce5804856c060184f0841c596c1896c84
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-10-09 19:04:33 +0530

    create session activity

--- app/src/main/java/com/netscape/utrain/activities/CreateTrainingSession.java
+++ app/src/main/java/com/netscape/utrain/activities/CreateTrainingSession.java
@@ -59,92 +59,90 @@ public class CreateTrainingSession extends AppCompatActivity {
         progressDialog.setMessage(getResources().getString(R.string.loading));
         progressDialog.setCancelable(false);
 
-        tvStartBsnsHour = findViewById(R.id.createTrainingSessionSelectStartbsnsHourTv);
-        tvEndBsnsHour = findViewById(R.id.createTrainingSessionSelectEndbsnsHourTv);
-        tvSelectDate = findViewById(R.id.createTrainingSessionEnterDateTv);
 
 
-        tvStartBsnsHour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar c = Calendar.getInstance();
-                mHour = c.get(Calendar.HOUR_OF_DAY);
-                mMinute = c.get(Calendar.MINUTE);
 
-                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateTrainingSession.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+//        tvStartBsnsHour.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar c = Calendar.getInstance();
+//                mHour = c.get(Calendar.HOUR_OF_DAY);
+//                mMinute = c.get(Calendar.MINUTE);
+//
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateTrainingSession.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+//
+//                        tvStartBsnsHour.setText(hourOfDay + ":" + minute);
+//                    }
+//                },mHour,mMinute,true);
+//                timePickerDialog.show();
+//            }
+//        });
 
-                        tvStartBsnsHour.setText(hourOfDay + ":" + minute);
-                    }
-                },mHour,mMinute,true);
-                timePickerDialog.show();
-            }
-        });
+//        tvEndBsnsHour.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar c = Calendar.getInstance();
+//                mHour = c.get(Calendar.HOUR_OF_DAY);
+//                mMinute = c.get(Calendar.MINUTE);
+//
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateTrainingSession.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+//
+//                        tvEndBsnsHour.setText(hourOfDay + ":" + minute);
+//                    }
+//                }, mHour, mMinute,true);
+//                timePickerDialog.show();
+//            }
+//        });
 
-        tvEndBsnsHour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar c = Calendar.getInstance();
-                mHour = c.get(Calendar.HOUR_OF_DAY);
-                mMinute = c.get(Calendar.MINUTE);
+//        tvSelectDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar c = Calendar.getInstance();
+//                mYear = c.get(Calendar.YEAR);
+//                mMonth = c.get(Calendar.MONTH);
+//                mDay = c.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateTrainingSession.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+//                        tvSelectDate.setText(dayOfMonth + "-" + (month +1) + "-" + year);
+//                    }
+//                }, mDay, mMonth, mYear);
+//                datePickerDialog.show();
+//            }
+//        });
 
-                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateTrainingSession.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-
-                        tvEndBsnsHour.setText(hourOfDay + ":" + minute);
-                    }
-                }, mHour, mMinute,true);
-                timePickerDialog.show();
-            }
-        });
-
-        tvSelectDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateTrainingSession.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                        tvSelectDate.setText(dayOfMonth + "-" + (month +1) + "-" + year);
-                    }
-                }, mDay, mMonth, mYear);
-                datePickerDialog.show();
-            }
-        });
-
-        spinnerLocation = findViewById(R.id.createTrainingSessionLocationSpinner);
-
-        List<String> list = new ArrayList<String>();
-        list.add("Select Location");
-        list.add("Texas");
-        list.add("America");
-        list.add("Australia");
-        list.add("England");
-        list.add("India");
-        list.add("Brazil");
-
-        ArrayAdapter adapter = new ArrayAdapter(CreateTrainingSession.this,android.R.layout.simple_spinner_item,list);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerLocation.setAdapter(adapter);
-
-        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                spinnerLocation.setSelection(i);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+//        spinnerLocation = findViewById(R.id.createTrainingSessionLocationSpinner);
+//
+//        List<String> list = new ArrayList<String>();
+//        list.add("Select Location");
+//        list.add("Texas");
+//        list.add("America");
+//        list.add("Australia");
+//        list.add("England");
+//        list.add("India");
+//        list.add("Brazil");
+//
+//        ArrayAdapter adapter = new ArrayAdapter(CreateTrainingSession.this,android.R.layout.simple_spinner_item,list);
+//
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerLocation.setAdapter(adapter);
+//
+//        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                spinnerLocation.setSelection(i);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
     }
     private void hitCreateEventApi() {
         progressDialog.show();
