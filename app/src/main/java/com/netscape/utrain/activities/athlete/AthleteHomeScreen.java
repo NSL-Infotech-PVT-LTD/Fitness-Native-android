862e62fab3bea1f45abb3bc39bd527da7b4f4518
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-10-11 19:07:57 +0530

    Drawer athlete home

--- app/src/main/java/com/netscape/utrain/activities/athlete/AthleteHomeScreen.java
+++ app/src/main/java/com/netscape/utrain/activities/athlete/AthleteHomeScreen.java
@@ -1,10 +1,14 @@
 package com.netscape.utrain.activities.athlete;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.netscape.utrain.R;
 import com.netscape.utrain.databinding.AActivityBottomNavigationBinding;
 import com.netscape.utrain.fragments.A_ChatsFragment;
@@ -18,13 +22,22 @@ import com.netscape.utrain.utils.PrefrenceConstant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Handler;
 import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,6 +45,10 @@ public class AthleteHomeScreen extends AppCompatActivity {
     private TextView mTextMessage;
     private AActivityBottomNavigationBinding binding;
     private boolean doubleBackToExitPressedOnce = false;
+    private AppBarConfiguration mAppBarConfiguration;
+    public static DrawerLayout drawer;
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -75,15 +92,57 @@ public class AthleteHomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.a_activity_bottom_navigation);
         binding = DataBindingUtil.setContentView(this, R.layout.a_activity_bottom_navigation);
-        setProfileImage();
-        loadFragment(new A_HomeFragment());
 
 //        BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         binding.navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setVisibility(View.GONE);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.slider);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
 
+        setProfileImage();
+//        loadFragment(new A_HomeFragment());
     }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.drawer_athlete_home, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+    @SuppressLint("WrongConstant")
+    public  static void openCloseDrawer(){
+        drawer.openDrawer(Gravity.START);
+    }
+
 
     private void setProfileImage() {
 
@@ -96,7 +155,7 @@ public class AthleteHomeScreen extends AppCompatActivity {
     private void loadFragment(Fragment fragment) {
         // load fragment
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.containerTwo, fragment);
+        transaction.replace(R.id.nav_host_fragment, fragment);
         transaction.commit();
     }
 
