b1045cb5aabeef577d0b7667cadaaa9dc4618c40
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-10-21 18:35:33 +0530

    ticket applied bottom sheed updated

--- app/src/main/java/com/netscape/utrain/adapters_org/O_BookedEventListAdapter.java
+++ app/src/main/java/com/netscape/utrain/adapters_org/O_BookedEventListAdapter.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.google.android.material.textview.MaterialTextView;
 import com.netscape.utrain.R;
+import com.netscape.utrain.model.BookedUserModel;
 import com.netscape.utrain.model.O_EventDataModel;
 import com.netscape.utrain.utils.Constants;
 
@@ -21,15 +22,20 @@ import org.json.JSONException;
 
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class O_BookedEventListAdapter extends RecyclerView.Adapter<O_BookedEventListAdapter.CustomTopCoachesHolder> {
 
     private Context context;
     private int previusPos = -1;
-    private List<O_EventDataModel> supplierData;
+    private List<BookedUserModel> supplierData;
 
-    public O_BookedEventListAdapter(Context context, List supplierData) {
+    onClick onClick;
+
+    public O_BookedEventListAdapter(Context context, List supplierData, onClick onClick) {
         this.context = context;
         this.supplierData = supplierData;
+        this.onClick = onClick;
 
     }
 
@@ -38,41 +44,30 @@ public class O_BookedEventListAdapter extends RecyclerView.Adapter<O_BookedEvent
     @Override
     public O_BookedEventListAdapter.CustomTopCoachesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_view, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_total_booked_user, parent, false);
         return new O_BookedEventListAdapter.CustomTopCoachesHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull O_BookedEventListAdapter.CustomTopCoachesHolder holder, int position) {
-        final O_EventDataModel data = supplierData.get(position);
-        try {
-            if (data.getImages() != null) {
-                JSONArray jsonArray = new JSONArray(data.getImages());
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    Glide.with(context).load(Constants.IMAGE_BASE_EVENT + jsonArray.get(i)).into(holder.eventImage);
+        final BookedUserModel data = supplierData.get(position);
 
-                }
-            }
+        holder.userName.setText(data.getUserName());
+        Glide.with(context).load(data.getUserImage()).into(holder.circleImageView);
 
-        } catch (JSONException e) {
-
-            Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-
-        }
-        holder.eventName.setText(data.getName());
-        holder.eventVenue.setText(data.getLocation());
-        holder.eventDate.setText(data.getStart_date());
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.viewDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-//                Intent topCoachesDetails = new Intent(context, TopCoachesDetailsActivity.class);
-////                topCoachesDetails.putExtra(Constants.TOP_DATA_INTENT,data);
-//                topCoachesDetails.putExtra(Constants.TOP_FROM_INTENT, "1");
-//                context.startActivity(topCoachesDetails);
+                onClick.onClick();
             }
         });
+
+
+    }
+
+    public interface onClick {
+        public void onClick();
     }
 
     @Override
@@ -82,16 +77,17 @@ public class O_BookedEventListAdapter extends RecyclerView.Adapter<O_BookedEvent
 
     public class CustomTopCoachesHolder extends RecyclerView.ViewHolder {
 
-        AppCompatImageView eventImage;
-        MaterialTextView eventName, eventVenue, eventDate;
+        CircleImageView circleImageView;
+        MaterialTextView userName, viewDetails;
+
 
         public CustomTopCoachesHolder(@NonNull View itemView) {
             super(itemView);
 
-            eventImage = itemView.findViewById(R.id.bookingEventImage);
-            eventName = itemView.findViewById(R.id.bookingEventName);
-            eventVenue = itemView.findViewById(R.id.bookingVenueTv);
-            eventDate = itemView.findViewById(R.id.bookingEventDate);
+            circleImageView = itemView.findViewById(R.id.bookedUserImg);
+            userName = itemView.findViewById(R.id.userName);
+            viewDetails = itemView.findViewById(R.id.viewDetails);
+
         }
     }
 
