1d7cd593778e3d6be2c3fa1b0ea4011bf7424647
chet.ram <chet.ram@netscapelabs.com>
2019-11-19 11:06:40 +0530

    transaction

--- app/src/main/java/com/netscape/utrain/activities/TransactionActivity.java
+++ app/src/main/java/com/netscape/utrain/activities/TransactionActivity.java
@@ -2,20 +2,21 @@ package com.netscape.utrain.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
 import com.netscape.utrain.R;
 import com.netscape.utrain.databinding.ActivityTransactionBinding;
-import com.netscape.utrain.fragments.O_CmpEventFragment;
-import com.netscape.utrain.fragments.O_HistoryFragment;
-import com.netscape.utrain.fragments.O_UpcEventFragment;
 import com.netscape.utrain.fragments.PaymentReceiveFragment;
 import com.netscape.utrain.fragments.PaymentSentFragment;
 
@@ -25,17 +26,18 @@ import java.util.List;
 public class TransactionActivity extends AppCompatActivity {
 
     ActivityTransactionBinding binding;
-    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_transaction);
         binding = DataBindingUtil.setContentView(TransactionActivity.this, R.layout.activity_transaction);
-        binding.transactionTabLayou.setTabTextColors(Color.parseColor("#D6D6D6"), Color.parseColor("#ffffff"));
+        binding.layoutTransactionTab.setTabTextColors(Color.parseColor("#D6D6D6"), Color.parseColor("#ffffff"));
+        binding.layoutTransactionTab.setupWithViewPager(binding.transactionViewPager);
         setupViewPager(binding.transactionViewPager);
-        binding.transactionTabLayou.setupWithViewPager(binding.transactionViewPager);
         transactionBackImageClick();
+        wrapTabIndicatorToTitle(binding.layoutTransactionTab, 100, 50);
+//        setupTabIcons();
 
     }
 
@@ -47,9 +49,12 @@ public class TransactionActivity extends AppCompatActivity {
             }
         });
     }
-
+    private void setupTabIcons() {
+        binding.layoutTransactionTab.getTabAt(0).setIcon(getResources().getDrawable(R.drawable.ic_arrow_left));
+        binding.layoutTransactionTab.getTabAt(1).setIcon(getResources().getDrawable(R.drawable.ic_arrow_left));
+    }
     private void setupViewPager(final ViewPager viewPager) {
-        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new PaymentSentFragment(), "Sent");
         adapter.addFragment(new PaymentReceiveFragment(), "Receive");
         viewPager.setAdapter(adapter);
@@ -80,8 +85,49 @@ public class TransactionActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
+
             return mFragmentTitleList.get(position);
         }
     }
+    public void wrapTabIndicatorToTitle(TabLayout tabLayout, int externalMargin, int internalMargin) {
+        View tabStrip = tabLayout.getChildAt(0);
+        if (tabStrip instanceof ViewGroup) {
+            ViewGroup tabStripGroup = (ViewGroup) tabStrip;
+            int childCount = ((ViewGroup) tabStrip).getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View tabView = tabStripGroup.getChildAt(i);
+                //set minimum width to 0 for instead for small texts, indicator is not wrapped as expected
+                tabView.setMinimumWidth(0);
+                // set padding to 0 for wrapping indicator as title
+                tabView.setPadding(0, tabView.getPaddingTop(), 0, tabView.getPaddingBottom());
+                // setting custom margin between tabs
+                if (tabView.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+                    ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) tabView.getLayoutParams();
+                    if (i == 0) {
+                        // left
+                        settingMargin(layoutParams, externalMargin, internalMargin);
+                    } else if (i == childCount - 1) {
+                        // right
+                        settingMargin(layoutParams, internalMargin, externalMargin);
+                    } else {
+                        // internal
+                        settingMargin(layoutParams, internalMargin, internalMargin);
+                    }
+                }
+            }
+            tabLayout.requestLayout();
+        }
+    }
+    private void settingMargin(ViewGroup.MarginLayoutParams layoutParams, int start, int end) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            layoutParams.setMarginStart(start);
+            layoutParams.setMarginEnd(end);
+        } else {
+            layoutParams.leftMargin = start;
+            layoutParams.rightMargin = end;
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/netscape/utrain/fragments/PaymentSentFragment.java
+++ app/src/main/java/com/netscape/utrain/fragments/PaymentSentFragment.java
@@ -7,17 +7,19 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.netscape.utrain.R;
+import com.netscape.utrain.databinding.PaymentSentFragmentBinding;
 
 public class PaymentSentFragment extends Fragment {
-
+    private PaymentSentFragmentBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.payment_sent_fragment,container,false);
-
+        binding = DataBindingUtil.inflate(inflater,R.layout.payment_sent_fragment,container,false);
+        View view=binding.getRoot();
         return view;
     }
 }
