5399c56c2fd56e36a5e93b30f982853680e55f3d
anil.netscapelabs <anil.kumar@netscapelabs.com>
2019-11-20 19:41:46 +0530

    working on notification api

---
+++ app/src/main/java/com/netscape/utrain/adapters/AllEventsListAdapter.java
@@ -0,0 +1,90 @@
+package com.netscape.utrain.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.textview.MaterialTextView;
+import com.netscape.utrain.R;
+import com.netscape.utrain.model.C_EventDataListModel;
+import com.netscape.utrain.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.List;
+
+public class AllEventsListAdapter extends RecyclerView.Adapter<AllEventsListAdapter.AllEventsListHolder> {
+
+    private Context context;
+    private List<C_EventDataListModel> list;
+
+    public AllEventsListAdapter(Context context, List<C_EventDataListModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+
+    @NonNull
+    @Override
+    public AllEventsListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_view, parent, false);
+        return new AllEventsListHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AllEventsListHolder holder, int position) {
+
+        C_EventDataListModel data = list.get(position);
+        holder.bookingEventName.setText(data.getName());
+        holder.bookingEventDate.setText(data.getStart_date());
+        holder.bookingVenueTv.setText(data.getLocation());
+        holder.bookingTicketTv.setText(data.getGuest_allowed()+"");
+
+        try {
+            if (data.getImages() != null) {
+                JSONArray jsonArray = new JSONArray(data.getImages());
+                if (jsonArray != null && jsonArray.length() > 0) {
+                    Glide.with(context).load(Constants.IMAGE_BASE_EVENT + jsonArray.get(0)).thumbnail(Glide.with(context).
+                            load(Constants.IMAGE_BASE_EVENT + Constants.THUMBNAILS + jsonArray.get(0))).into(holder.bookingEventImage);
+                }
+            }
+
+        } catch (JSONException e) {
+
+            Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class AllEventsListHolder extends RecyclerView.ViewHolder {
+
+        MaterialTextView bookingEventName, bookingEventDate, bookingVenueTv, bookingTicketTv;
+        AppCompatImageView bookingEventImage;       // Using the booking.view layout which is same for completed....
+
+        public AllEventsListHolder(@NonNull View itemView) {
+            super(itemView);
+
+            bookingEventName = itemView.findViewById(R.id.bookingEventName);
+            bookingEventDate = itemView.findViewById(R.id.bookingEventDate);
+            bookingVenueTv = itemView.findViewById(R.id.bookingVenueTv);
+            bookingTicketTv = itemView.findViewById(R.id.bookingTicketTv);
+            bookingEventImage = itemView.findViewById(R.id.bookingEventImage);
+
+        }
+    }
+}
--- app/src/main/java/com/netscape/utrain/fragments/AllEventsFragment.java
+++ app/src/main/java/com/netscape/utrain/fragments/AllEventsFragment.java
@@ -1,21 +1,54 @@
 package com.netscape.utrain.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.netscape.utrain.R;
+import com.netscape.utrain.adapters.AllEventsListAdapter;
 import com.netscape.utrain.databinding.AllEventsFragmentBinding;
+import com.netscape.utrain.model.C_EventDataListModel;
+import com.netscape.utrain.response.C_EventListResponse;
+import com.netscape.utrain.retrofit.RetrofitInstance;
+import com.netscape.utrain.retrofit.Retrofitinterface;
+import com.netscape.utrain.utils.CommonMethods;
+import com.netscape.utrain.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AllEventsFragment extends Fragment {
 
+
+    private Retrofitinterface retrofitinterface;
     private AllEventsFragmentBinding binding;
+    private Context context;
+    private ProgressDialog progressDialog;
+    private List<C_EventDataListModel> eventList = new ArrayList<>();
+    private RecyclerView allEventListRecycler;
+    private RecyclerView.LayoutManager layoutManager;
+    private AllEventsListAdapter eventAdapter;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
 
     @Nullable
     @Override
@@ -24,13 +57,44 @@ public class AllEventsFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.all_events_fragment, container, false);
         View view = binding.getRoot();
 
+        layoutManager = new LinearLayoutManager(context);
+        binding.allEventListRecycler.setLayoutManager(layoutManager);
+        AllEventsCreatedApi();
+
 
         return view;
     }
 
-    private void AllEventsCreatedApi() {
+    public void AllEventsCreatedApi() {
 
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
 
+        retrofitinterface = RetrofitInstance.getClient().create(Retrofitinterface.class);
+        Call<C_EventListResponse> callEventCoachList =
+                retrofitinterface.getCoachEventList("Bearer " + CommonMethods.getPrefData(Constants.AUTH_TOKEN, context), Constants.CONTENT_TYPE, "");
 
+        callEventCoachList.enqueue(new Callback<C_EventListResponse>() {
+            @Override
+            public void onResponse(Call<C_EventListResponse> call, Response<C_EventListResponse> response) {
+                progressDialog.dismiss();
+                if (response.isSuccessful()) {
+                    if (response.body().isStatus())
+                        if (response.body() != null) {
+                            eventList = response.body().getData().getData();
+                            eventAdapter = new AllEventsListAdapter(context, eventList);
+                            binding.allEventListRecycler.setAdapter(eventAdapter);
+                        }
+                } else {
+                    Toast.makeText(context, "" + response.body().getError().getError_message(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<C_EventListResponse> call, Throwable t) {
+                progressDialog.dismiss();
+            }
+        });
     }
 }
